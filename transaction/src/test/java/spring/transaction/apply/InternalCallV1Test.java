8a0953e04b232061c9d72507d17d543960b69c63
isyoo1213 <moethns@gmail.com>
2023-03-11 18:23:33 +0900

    *** feat: Add Comments about 'this' call

--- transaction/src/test/java/spring/transaction/apply/InternalCallV1Test.java
+++ transaction/src/test/java/spring/transaction/apply/InternalCallV1Test.java
@@ -16,6 +16,8 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
  *   1. @Transactional 어노테이션이 내부에 하나라도 존재하면 우선적으로 Transaction AOP가 target 클래스의 Proxy를 생성/Bean등록/주입
  *   2. @Transactional 어노테이션의 여부 확인 전에, 호출되는 것은 주입된 Proxy
  *   3. Proxy에서 @Transactional 어노테이션 여부 확인을 통해 Transaction 생성 판단 후 target 클래스 인스턴스의 실제 로직 호출
+ * * 핵심은 this
+ *   - 메서드의 호출에 특정한 참조가 없다면 자기 자신의 내부 메서드를 호출하는 것
  */
 
 @Slf4j
@@ -64,6 +66,8 @@ class InternalCallV1Test {
             //Tx관련 info를 출력한 후 internal()을 호출하도록 가정
             // *** Transaction이 필요한 부분
             // *** but, Proxy를 거치지 않고 target 클래스 인스턴스가 직접 호출하는 것이 문제 상황
+            // *** 모든 메서드의 호출에서 앞에 별도의 '참조'가 없으면 this.메서드()의 형식으로 호출된다
+            // -> 즉, 자기 자신의 내부 메서드를 호출
             // *** 즉 this.internal(), 참조(callService 인스턴스)를 거치지 않고 나 자신(실제 target 클래스)의 인스턴스를 호출하는 것
             // *** -> '내부호출'은 Proxy를 거치지 않는다 -> Transaction을 적용할 수 없다(Transaction은 AOP를 통해 Proxy에서 처리하므로)
             internal();
