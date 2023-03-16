8b766441c1c1aa523626bafaad8abc7571fceb28
isyoo1213 <moethns@gmail.com>
2023-03-16 23:04:02 +0900

    feat: Add Test for member/log Repo

--- transaction/src/main/java/spring/transaction/propagation/LogRepository.java
+++ transaction/src/main/java/spring/transaction/propagation/LogRepository.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Slf4j
 @Repository
 @RequiredArgsConstructor
@@ -22,5 +24,11 @@ public class LogRepository {
             log.info("log 저장 시 예외 발생");
             throw new RuntimeException("예외 발생");
         }
+    }
+
+    public Optional<Log> find(String message) {
+        return em.createQuery("select l from Log l where l.message = :message", Log.class)
+                .setParameter("message", message)
+                .getResultList().stream().findAny();
     }
 }
---
+++ transaction/src/test/java/spring/transaction/propagation/MemberServiceTest.java
@@ -0,0 +1,43 @@
+package spring.transaction.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    LogRepository logRepository;
+
+    /**
+     * memberService     @Transactional:OFF
+     * memberRepository  @Transactional:ON
+     * logRepository     @Transactional:ON
+     */
+    @Test
+    void outerTxOff_success() {
+        //given
+        String username = "outerTxOff_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then : 모든 데이터가 정상 저장된다
+
+        //Assertions - junit의 Assertions를 사용
+        // -> Optional 처리 편리하게 가능
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent()); //Log의 message는 username과 동일하게 맞추기로
+    }
+
+}
