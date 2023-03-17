e37ca0085bae8b78b1f8fde6f25f27c65c9b3e35
isyoo1213 <moethns@gmail.com>
2023-03-17 13:56:16 +0900

    feat: Add Test for Each-Tx / Commit-Rollback

--- transaction/src/main/java/spring/transaction/propagation/MemberRepository.java
+++ transaction/src/main/java/spring/transaction/propagation/MemberRepository.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -19,7 +20,7 @@ public class MemberRepository {
 
     private final EntityManager em;
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void save(Member member) {
         log.info("member 저장");
         em.persist(member);
--- transaction/src/test/java/spring/transaction/propagation/MemberServiceTest.java
+++ transaction/src/test/java/spring/transaction/propagation/MemberServiceTest.java
@@ -1,11 +1,11 @@
 package spring.transaction.propagation;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -38,6 +38,28 @@ class MemberServiceTest {
         // -> Optional 처리 편리하게 가능
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent()); //Log의 message는 username과 동일하게 맞추기로
+    }
+
+    /**
+     * memberService     @Transactional:OFF
+     * memberRepository  @Transactional:ON
+     * logRepository     @Transactional:ON + Exception(RuntimeException)
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail"; // -> contains "로그예외" -> logRepository에서의 RuntimeException
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //then : 모든 데이터가 정상 저장된다
+
+        //Assertions - junit의 Assertions를 사용
+        // -> Optional 처리 편리하게 가능
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty()); //Log의 message는 username과 동일하게 맞추기로
     }
 
 }
